957d22ef68f74178a1ca37d1572cb5791459b8bb
tavhu <thythona168@gmail.com>
2023-10-29 18:23:58 +0700

    tests

--- layouts/front.vue
+++ layouts/front.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Html } from '#ui-colors/components';
 import { useToast,TwToast } from 'vue3-tailwind';
 import {useThemeStore} from '~~/store/theme'
 
@@ -10,13 +11,34 @@ useHead({
 const { toasts } = useToast();
 const theme = useThemeStore()
 const isDark = computed(()=>theme.isDark)
+const HTML :any = ref()
+
+onMounted(()=>{
+    HTML.value = document.querySelector('body')
+     if (isDark.value) {
+        HTML.value?.classList.add('bg-gray-500')
+      } else {
+        HTML.value?.classList.remove('bg-gray-500')
+      }
+})
+
+
+watch(isDark, () => {
+  console.log(isDark)
+  if (isDark.value) {
+    HTML.value?.classList.add('bg-gray-500')
+  } else {
+    HTML.value?.classList.remove('bg-gray-500')
+  }
+})
+
 </script>
 
 <template>
   <div id="dark-mode"  :class="isDark ? 'dark vt-dark' : ''">
     <TwToast position="top-right" :toasts="toasts" />
     <LayoutFloating />
-    <div class="px-0 md:px-3 lg:px-4">
+    <div class="px-0 md:px-3 lg:px-4" >
       <slot />
     </div>
   </div>
