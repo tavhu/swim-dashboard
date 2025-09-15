1926c534059114d7353098dc1c873a7461eb5c35
tavhu <thythona168@gmail.com>
2025-09-15 14:12:01 +0700

    fixed side bar position

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
 </script>
-
 <template>
-  <nav class="hidden md:block lg:w-64 sm:w-32 text-md text-secondary p-6 py-10 h-full sticky top-10 z-10">
-    <LayoutSidebarList />
+  <nav class="hidden md:block lg:w-64 sm:w-32 text-md text-secondary relative top-5 z-20">
+    <LayoutSidebarList class="fixed" />
   </nav>
-
 </template>
-
-
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 
-import {  useThemeStore } from '~~/store/theme'
+import { useThemeStore } from '~~/store/theme'
 
 import { useToast, TwFeather, TwToast } from "vue3-tailwind";
 const { toasts } = useToast();
@@ -8,12 +8,12 @@ const { toasts } = useToast();
 // const cookieDark = useCookie('dark-mode')
 
 const theme = useThemeStore()
-const isDark = computed(()=>theme.isDark)
+const isDark = computed(() => theme.isDark)
 // console.log(theme.appTheme)
 </script>
 
 <template>
-  <div id="dark-mode"  :class="isDark ? 'dark vt-dark' : ''">
+  <div id="dark-mode" :class="isDark ? 'dark vt-dark' : ''">
     <NuxtLoadingIndicator />
     <TwToast position="top-right" :toasts="toasts" />
     <div>
@@ -23,7 +23,7 @@ const isDark = computed(()=>theme.isDark)
         <div class="flex min-h-screen text-slate-900 dark:text-white  dark:bg-gray-900 bg-gray-100  ">
           <div class="h-screen border-r-2 flex flex-col justify-between">
             <LayoutSidebar />
-            <LayoutSidebar_bottom />  
+            <LayoutSidebar_bottom />
           </div>
           <div class="w-full md:w-[calc(100%-8rem)] lg:w-[calc(100%-16rem)] sm:rounded-l-none">
             <div class="p-4 bg-gray-100 dark:bg-gray-900 h-full  font-[battambang]">
